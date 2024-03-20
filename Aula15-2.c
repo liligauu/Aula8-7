19c12b281ece868b141d3168d063ad43cb5fdbe3
Liligau <145306106+liligauu@users.noreply.github.com>
2024-03-20 20:05:46 -0300

    Update Aula15-2.c

--- Aula15-2.c
+++ Aula15-2.c
@@ -3,15 +3,6 @@
 #include <string.h>
 #include <math.h>
 
-/*
-2) Crie um programa de cadastro que receba, armazene, e
-em seguida, exiba os dados de 5 pessoas.
- Cada pessoa possui: nome, idade, peso, data de nascimento,
-brasileiro ou estrangeiro. Caso seja Brasileiro, armazene o CPF,
-caso estrangeiro, armazene o passaporte.
- Regra: Utilize structs, typedef, union e enum.
-*/
-
 typedef enum{Brasileiro = 1, Estrangeiro = 2} Option;
 
 typedef struct{
@@ -22,7 +13,7 @@ typedef struct{
 
 typedef union{
     int CPF[12];
-    int Passaporte[7];
+    char Passaporte[10]; 
 } Documentos;
 
 int main(){
@@ -31,39 +22,49 @@ int main(){
     Cadastro cadastro[5];
     Documentos nacionalidade[5];
 
-for(int i = 0; i < 5; i++){
-    printf("Digite o seu nome: ");
-    fgets(cadastro[i].Nome, 41, stdin);
-    cadastro[i].Nome[strcspn(cadastro[i].Nome, "\n")] = '\0';
-    setbuf(stdin, NULL);
-    printf("Digite sua idade: ");
-    scanf("%i", &cadastro[i].Idade);
-    printf("Digite seu peso (KG): ");
-    scanf("%f", &cadastro[i].Peso);
-    setbuf(stdin, NULL);
-    printf("Escolha uma opção: \n");
-    printf("1 - Brasileiro\n");
-    printf("2 - Estrangeiro\n");
-    scanf("%i", &option);
-    switch(option){
-        case 1: 
-        printf("Digite o seu CPF: ");
-        scanf("%i", &nacionalidade[i].CPF);
+    for(int i = 0; i < 5; i++){
+        printf("Digite o seu nome: ");
+        fgets(cadastro[i].Nome, 41, stdin);
+        cadastro[i].Nome[strcspn(cadastro[i].Nome, "\n")] = '\0';
         setbuf(stdin, NULL);
-        break;
-        case 2:
-        printf("Digite os números seu Passaporte: ");
-        scanf("%i", &nacionalidade[i].Passaporte);
+        printf("Digite sua idade: ");
+        scanf("%i", &cadastro[i].Idade);
+        printf("Digite seu peso (KG): ");
+        scanf("%f", &cadastro[i].Peso);
         setbuf(stdin, NULL);
-        break;
-        default:
-        break;
+        printf("Escolha uma opção: \n");
+        printf("1 - Brasileiro\n");
+        printf("2 - Estrangeiro\n");
+        scanf("%i", &option);
+        setbuf(stdin, NULL);
+        switch(option){
+            case 1: 
+                printf("Digite o seu CPF: ");
+                scanf("%i", &nacionalidade[i].CPF);
+                setbuf(stdin, NULL);
+                break;
+            case 2:
+                printf("Digite o número do seu Passaporte: ");
+                fgets(nacionalidade[i].Passaporte, 10, stdin);
+                nacionalidade[i].Passaporte[strcspn(nacionalidade[i].Passaporte, "\n")] = '\0';
+                setbuf(stdin, NULL);
+                break;
+            default:
+                break;
+        }
     }
-    
-}
 
-
-
+    printf("\nDados cadastrados das 5 pessoas:\n");
+    for(int i = 0; i < 5; i++){
+        printf("\n----- Pessoa %d -----\n", i+1);
+        printf("Nome: %s\n", cadastro[i].Nome);
+        printf("Idade: %d\n", cadastro[i].Idade);
+        printf("Peso: %.2f\n", cadastro[i].Peso);
+        if(option == Brasileiro)
+            printf("CPF: %i\n", nacionalidade[i].CPF);
+        else if(option == Estrangeiro)
+            printf("Passaporte: %s\n", nacionalidade[i].Passaporte);
+    }
 
     return 0;
 }
